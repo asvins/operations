058d4739e2e6b2764c0a1746f070099dd5c03038
Vinicius Correa <viniciuscorrea91@gmail.com>
2015-12-08 13:42:06 -0200

    Extracted models

---
+++ models/pack.go
@@ -0,0 +1,22 @@
+package models
+
+import "time"
+
+const (
+	PackStatusDelivered = iota
+	PackStatusShipped
+	PackStatusOnProduction
+	PackStatusScheduled
+)
+
+type Pack struct {
+	ID           int       `json:"pack_id" gorm:"column:id"`
+	Owner        string    `json:"owner"`
+	Supervisor   string    `json:"supervisor"`
+	From         time.Time `json:"from" gorm:"column:from_date"`
+	To           time.Time `json:"to" gorm:"column:to_date"`
+	TrackingCode string    `json:"tracking_code"`
+	Status       int       `json:"Status"`
+	PackType     string    `json:"pack_type"`
+	PackHash     string    `json:"hash"`
+}
